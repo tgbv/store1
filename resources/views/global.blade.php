42c7de8efc7d647df51e29e2934e0b7750611a62
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2019-05-24 22:55:22 +0300

    Added meta

--- resources/views/global.blade.php
+++ resources/views/global.blade.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>@yield('title')</title>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">		
