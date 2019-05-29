dd55c89e09d89ad435c6d5f9bbe9fc60b8834df2
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2019-05-29 17:18:11 +0300

    Fixes

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;	
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Session;
+
 use Illuminate\Database\Eloquent\Model as MEloquent;
 use App\Orders as MOrders;
 
--- database/migrations/2019_05_29_103508_orders.php
+++ database/migrations/2019_05_29_103508_orders.php
@@ -24,6 +24,7 @@ class Orders extends Migration
             $table -> string('address');
             $table -> string('phone');
             $table -> text('products');
+            $table -> string('status');
             $table -> timestamps();
         });
     }
