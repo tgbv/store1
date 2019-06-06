0644a05d9b30dfd4db1adf5cae57102054cb6be4
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2019-06-07 00:30:34 +0300

    fixes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+*	Set timezone for production
+*/
+date_default_timezone_set("Europe/Bucharest");
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
--- resources/views/area51/orders.blade.php
+++ resources/views/area51/orders.blade.php
@@ -2,7 +2,7 @@
 	
 	use \Carbon\Carbon;
 
-	date_default_timezone_set("UTC");
+	//date_default_timezone_set("UTC");
 
 ?>
 @extends('area51.global')
