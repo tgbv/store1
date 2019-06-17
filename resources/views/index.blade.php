aef8a4c331b4ab328653150945f2ee521dd9a79a
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2019-06-17 17:30:59 +0300

    Fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -236,13 +236,31 @@
 			</div>
 		</div>
 
-		@if(count($PRODUCTS) > 0)
+		@if((count($TALL) > 0 || count($SHORT) > 0))
 			<!-- products card-->
 			<div id="my_work" name="my_work"></div>
 			<div class="__mywork card-panel grey lighten-5 z-depth-1 hide-on-small-only">
 				<div class="__my_work">My work</div>
 				<div class="row">
-					@foreach($PRODUCTS as $object)
+					@foreach($SHORT as $object)
+					    <div class="col s12 m4 l4 ">
+					      <div class="card hoverable">
+					        <div class="card-image ">
+					          <img alt="{{ $object->title }}" class="responsive-img materialboxed" src="/static/{{$object -> fname }}">
+					        </div>
+					        <div class="card-content">
+					        	<span class="card-title">{{$object -> title}}</span>
+					        </div>
+					        <div class="card-action">
+					        	<a class="blue-text waves-effect" href="/products/{{$object -> id}}#product">Details</a>
+					        	<a class="red-text waves-effect" href="/cart/{{$object -> id}}/add">Add to cart</a>
+					        </div>
+					      </div>
+					    </div>
+					@endforeach
+				</div>
+				<div class="row">
+					@foreach($TALL as $object)
 					    <div class="col s12 m4 l4 ">
 					      <div class="card hoverable">
 					        <div class="card-image ">
@@ -264,7 +282,26 @@
 			<div class="__mywork_mobile hide-on-med-and-up">
 				<div class="row">
 					<div class="__my_work">My work</div>
-					@foreach($PRODUCTS as $object)
+					@foreach($SHORT as $object)
+					    <div class="col s12 m4 l4 ">
+					      <div class="card hoverable">
+					        <div class="card-image ">
+					          <img alt="{{$object -> title}}" class="responsive-img materialboxed" src="/static/{{$object -> fname }}">
+					        </div>
+					        <div class="card-content">
+					        	<span class="card-title">{{$object -> title}}</span>
+					        </div>
+
+					        <div class="card-action">
+					        	<a class="blue-text waves-effect" href="/products/{{$object -> id}}#product">Details</a>
+					        	<a class="red-text waves-effect" href="/cart/{{$object -> id}}/add">Add to cart</a>
+					        </div>
+					    	</div>
+					    </div>
+					@endforeach
+				</div>
+					<div class="__my_work">My work</div>
+					@foreach($TALL as $object)
 					    <div class="col s12 m4 l4 ">
 					      <div class="card hoverable">
 					        <div class="card-image ">
